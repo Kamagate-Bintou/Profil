773adac6ca7aa30f660f76205d3aa1f4744fd81b
bintou <kbintou2019@gmail.com>
2023-03-08 00:05:23 +0100

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+
+class ProfileController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+  
+    public function index()
+    {
+        return view('profile');
+    }
+
+  
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'avatar' => 'required|image',
+        ]);
+  
+        $avatarName = time().'.'.$request->avatar->getClientOriginalExtension();
+        $request->avatar->move(public_path('avatars'), $avatarName);
+  
+        Auth()->user()->update(['avatar'=>$avatarName]);
+  
+        return back()->with('success', 'Avatar updated successfully.');
+    }
+}
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Profile') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('profile_store') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        @if (session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                        @endif
+
+                        <div class="row mb-3">
+                            <label for="username" class="col-sm-3 offset-sm-1 col-form-label"><B>Nom du
+                                    juge*</B></label>
+                            <div class="col-sm-7">
+                                <input type="text" class="form-control @if ($errors->has('username')) is-invalid @endif"
+                                    id="username" name="username" value="{{ old('username') }}">
+                                @if($errors->has('username'))
+                                <span class="text-danger">
+                                    {{$errors->first('username')}}
+                                </span>
+                                @endif
+                            </div>
+
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-sm-3 offset-sm-1 col-form-label"><B>Email*</B></label>
+                            <div class="col-sm-7">
+                                <input type="text" class="form-control @if ($errors->has('email')) is-invalid @endif"
+                                    id="email" name="email" value="{{ old('email') }}">
+                                @if($errors->has('email'))
+                                <span class=" text-danger">
+                                    {{$errors->first('email')}}
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-sm-3 offset-sm-1 col-form-label"><B>Mot de
+                                    passe*</B></label>
+                            <div class="col-sm-7">
+                                <input type="text" class="form-control @if ($errors->has('password')) is-invalid @endif"
+                                    id="password" name="password" value="{{ old('password') }}">
+                                @if($errors->has('password'))
+                                <span class=" text-danger">
+                                    {{$errors->first('password')}}
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="avatar" class="col-md-4 col-form-label text-md-end">{{ __('Avatar') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="avatar" type="file"
+                                    class="form-control @error('avatar') is-invalid @enderror" name="avatar"
+                                    value="{{ old('avatar') }}" required autocomplete="avatar">
+
+                                <img src="/avatars/{{ Auth::user()->avatar }}" style="width:80px;margin-top: 10px;">
+
+                                @error('avatar')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-dark">
+                                    {{ __('Upload Profile') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
